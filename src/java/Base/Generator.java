eb1d21e9af0d2b0589691d9a092f7b14ab3db318
hhientrong <hotronghien2k5@gmail.com>
2025-06-01 13:46:00 +0700

    Create password generator

--- src/java/Base/Generator.java
+++ src/java/Base/Generator.java
@@ -5,6 +5,9 @@
 package Base;
 
 import java.security.SecureRandom;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 import java.util.UUID;
 
 /**
@@ -12,6 +15,12 @@ import java.util.UUID;
  * @author Huyen
  */
 public class Generator {
+
+    private static final String LETTERS = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    private static final String NUMBERS = "0123456789";
+    private static final String SPECIALS = "!@#";
+
+    private static final String ALL_CHARS = LETTERS + NUMBERS + SPECIALS;
 
     private static String generate(String prefix, int length) {
         int totalLength = length;
@@ -25,6 +34,29 @@ public class Generator {
 
         String uuidPart = cleanUUID.substring(0, Math.min(uuidPartLength, cleanUUID.length()));
         return prefix + "-" + uuidPart;
+    }
+
+    public static String generatePassword(int length) {
+        SecureRandom random = new SecureRandom();
+
+        List<Character> passwordChars = new ArrayList<>();
+
+        passwordChars.add(LETTERS.charAt(random.nextInt(LETTERS.length())));
+        passwordChars.add(NUMBERS.charAt(random.nextInt(NUMBERS.length())));
+        passwordChars.add(SPECIALS.charAt(random.nextInt(SPECIALS.length())));
+
+        for (int i = 3; i < length; i++) {
+            passwordChars.add(ALL_CHARS.charAt(random.nextInt(ALL_CHARS.length())));
+        }
+
+        Collections.shuffle(passwordChars);
+
+        StringBuilder password = new StringBuilder();
+        for (char c : passwordChars) {
+            password.append(c);
+        }
+
+        return password.toString();
     }
 
     public static String generateOTP(int length) {
@@ -60,6 +92,6 @@ public class Generator {
     }
 
     public static void main(String[] args) {
-        System.out.println(generateOTP());
+        System.out.println(generatePassword(8));
     }
 }
