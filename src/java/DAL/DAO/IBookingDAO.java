6c067a1567b8e344672fd8860f4065d340e3a4f5
hhientrong <hotronghien2k5@gmail.com>
2025-06-30 16:02:10 +0700

    Update booking Dao

---
+++ src/java/DAL/DAO/IBookingDAO.java
@@ -0,0 +1,30 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package DAL.DAO;
+
+import Model.Booking;
+import java.sql.Date;
+
+/**
+ *
+ * @author Hien
+ */
+public interface IBookingDAO {
+
+    //GET
+    public Booking getBookingViaCheckinAndCheckOutDate(Date checkIn, Date checkOut);
+
+    //ADD
+    public boolean addBooking(Booking b);
+    
+    //UPDATE
+    public boolean updateBooking(Booking b);
+    
+    public boolean updateBookingStatus(String bookingId, int statusId);
+    
+    public boolean isRoomAvailable(String roomId, Date checkIn, Date checkOut);
+    
+    public boolean isHouseAvailable(String houseId, Date checkIn, Date checkOut);
+}
